29bdc3bd57aa2a6c5b712a35ceed919a0487e844
Max <max@amaden.net>
2023-01-24 01:16:35 +0000

    Recommit of auto-calculate .mjs

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -45,14 +45,58 @@ export class Zenithal2eActor extends Actor {
   _prepareCharacterData(actorData) {
     if (actorData.type !== 'character') return;
 
-    // Make modifications to data here. For example:
     const systemData = actorData.system;
 
-    // Loop through ability scores, and add their modifiers to our sheet output.
-    for (let [key, ability] of Object.entries(systemData.abilities)) {
-      // Calculate the modifier using d20 rules.
-      ability.mod = Math.floor((ability.value - 10) / 2);
-    }
+    // Calculate attribute points spent.
+    let pointsToBeSpent = 2 + (systemData.rank.value * 2);
+    pointsToBeSpent -= systemData.attributes.durability.value;
+    pointsToBeSpent -= systemData.attributes.mass.value;
+    pointsToBeSpent -= systemData.attributes.agility.value;
+    pointsToBeSpent -= systemData.attributes.precision.value;
+    pointsToBeSpent -= systemData.attributes.cunning.value;
+
+    if (pointsToBeSpent == 0) systemData.pointsRemainingMessage = " ";
+    else if (pointsToBeSpent < 0) systemData.pointsRemainingMessage = "[" + Math.abs(pointsToBeSpent) + "] too many points!"
+    else systemData.pointsRemainingMessage = "[" + pointsToBeSpent + "] points remaining.";
+    systemData.pointsRemainingMessage = "<p style=\"text-align:center; font-weight:bold\">" + systemData.pointsRemainingMessage + "</p>";
+
+    // Calculate Resources.
+    systemData.life.max = (50 * systemData.rank.value) + (5 * systemData.attributes.durability.value) + (2 * systemData.attributes.mass.value) + (1 * systemData.attributes.agility.value);
+    if (systemData.armor.type == "armor_light") { systemData.life.max += 10 * systemData.rank.value; }
+    else if (systemData.armor.type == "armor_medium") { systemData.life.max += 20 * systemData.rank.value; }
+    else if (systemData.armor.type == "armor_heavy") { systemData.life.max += 30 * systemData.rank.value; }
+    else if (systemData.armor.type == "packs_medium") { systemData.life.max += 5 * systemData.rank.value; }
+    else if (systemData.armor.type == "packs_heavy") { systemData.life.max += 15 * systemData.rank.value; }
+    else if (systemData.armor.type == "robes_light") { systemData.life.max -= 10 * systemData.rank.value; }
+    else if (systemData.armor.type == "robes_heavy") { systemData.life.max -= 25 * systemData.rank.value; }
+
+    // Calculate Properties.
+    systemData.damageMultiplier.value = (50) + (50 * systemData.rank.value);
+
+    systemData.accuracy.value = (2 * systemData.rank.value) + (1 * systemData.attributes.precision.value);
+
+    systemData.healingFactor.value = (10 * systemData.rank.value) + (2 * systemData.attributes.durability.value);
+
+    systemData.speed.value = (25) + Math.floor(systemData.attributes.agility.value / 2);
+    if (systemData.armor.type == "armor_medium" && systemData.attributes.mass.value <= 1) { systemData.speed.value -= 5; }
+    else if (systemData.armor.type == "armor_heavy" && systemData.attributes.mass.value <= 1) { systemData.speed.value -= 10; }
+    else if (systemData.armor.type == "armor_heavy" && systemData.attributes.mass.value <= 3) { systemData.speed.value -= 5; }
+
+    systemData.dodgeRating.value = (systemData.attributes.agility.value);
+    if (systemData.armor.type == "bare") { systemData.dodgeRating.value += 11 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "clothes") { systemData.dodgeRating.value += 10 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "armor_light") { systemData.dodgeRating.value += 7 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "armor_medium") { systemData.dodgeRating.value += 4 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "armor_heavy") { systemData.dodgeRating.value += 1 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "packs_light") { systemData.dodgeRating.value += 9 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "packs_medium") { systemData.dodgeRating.value += 7 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "packs_heavy") { systemData.dodgeRating.value += 4 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "robes_light") { systemData.dodgeRating.value += 7 + (2 * systemData.rank.value); }
+    else if (systemData.armor.type == "robes_heavy") { systemData.dodgeRating.value += 4 + (2 * systemData.rank.value); }
+
+    systemData.criticalResistance.value = 0;
+    if (systemData.armor.type == "armor_medium") { systemData.criticalResistance.value += 1; }
+    else if (systemData.armor.type == "armor_heavy") { systemData.criticalResistance.value += 2; }
   }
 
   /**
